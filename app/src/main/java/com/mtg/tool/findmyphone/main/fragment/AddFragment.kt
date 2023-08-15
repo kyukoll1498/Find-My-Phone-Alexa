2fc78ccc767b3ceae463f4da2d0b55616456e1f8
NhatYugi <nhatnhatth5@gmail.com>
2023-08-15 09:26:29 +0700

    update Add Audio Screen

--- app/src/main/java/com/mtg/tool/findmyphone/main/fragment/AddFragment.kt
+++ app/src/main/java/com/mtg/tool/findmyphone/main/fragment/AddFragment.kt
@@ -1,15 +1,25 @@
 package com.mtg.tool.findmyphone.main.fragment
 
+import android.view.View
 import com.mtg.tool.findmyphone.base.BaseFragment
 import com.mtg.tool.findmyphone.databinding.FragmentAddBinding
-import com.mtg.tool.findmyphone.databinding.FragmentHomeBinding
 
 class AddFragment  : BaseFragment<FragmentAddBinding>(FragmentAddBinding::inflate) {
     override fun initView() {
+    }
 
+    private fun setUpResponsive() {
+        if (binding.tvEmpty.y + binding.tvEmpty.height > binding.ivGuideCreate.y) {
+            binding.ivGuideCreate.visibility = View.GONE
+        }
     }
 
     override fun addEvent() {
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setUpResponsive()
     }
 }
