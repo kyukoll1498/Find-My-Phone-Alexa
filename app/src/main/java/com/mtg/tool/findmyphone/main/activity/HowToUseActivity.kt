701bc3cbf68f4b8e30a565dca43ee065785fb3bb
NhatYugi <nhatnhatth5@gmail.com>
2023-08-22 14:18:49 +0700

    add collapsible banner

--- app/src/main/java/com/mtg/tool/findmyphone/main/activity/HowToUseActivity.kt
+++ app/src/main/java/com/mtg/tool/findmyphone/main/activity/HowToUseActivity.kt
@@ -1,11 +1,14 @@
 package com.mtg.tool.findmyphone.main.activity
 
+import com.common.control.manager.AdmobManager
+import com.mtg.tool.findmyphone.BuildConfig
 import com.mtg.tool.findmyphone.base.BaseActivity
 import com.mtg.tool.findmyphone.databinding.ActivityCreateSoundBinding
 import com.mtg.tool.findmyphone.databinding.ActivityHowToUseBinding
 
 class HowToUseActivity: BaseActivity<ActivityHowToUseBinding>(ActivityHowToUseBinding::inflate) {
     override fun initView() {
+        AdmobManager.getInstance().loadCollapsibleBanner(this, BuildConfig.collapsible_banner_how_to_use, binding.frAd)
 
     }
 
