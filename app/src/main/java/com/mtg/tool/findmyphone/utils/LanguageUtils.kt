6e5ba26bb0bf7ce8d5d9be6550d41c17a4c93b2e
TuanAnh <tuananhxmen369@gmail.com>
2023-08-23 15:43:23 +0700

    Update policy

--- app/src/main/java/com/mtg/tool/findmyphone/utils/LanguageUtils.kt
+++ app/src/main/java/com/mtg/tool/findmyphone/utils/LanguageUtils.kt
@@ -28,7 +28,7 @@ object LanguageUtils {
             )
             mList.add(ItemLanguage(R.drawable.flag_cs, "Czech", R.drawable.ic_disable, "cs"))
             mList.add(ItemLanguage(R.drawable.flag_pl, "Polish", R.drawable.ic_disable, "pl"))
-            mList.add(ItemLanguage(R.drawable.flag_nl, "Dutch", R.drawable.ic_disable, "ru"))
+            mList.add(ItemLanguage(R.drawable.flag_nl, "Dutch", R.drawable.ic_disable, "nl"))
             mList.add(
                 ItemLanguage(
                     R.drawable.flag_fr,
