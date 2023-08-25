66e863c8a4815971df7d71fc61efd7411cc57bbd
TuanAnh <tuananhxmen369@gmail.com>
2023-08-25 09:33:39 +0700

    fix notification turn off feature clap

--- app/src/main/java/com/mtg/tool/findmyphone/main/activity/MainActivity.kt
+++ app/src/main/java/com/mtg/tool/findmyphone/main/activity/MainActivity.kt
@@ -200,6 +200,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         super.onNewIntent(intent)
         if (intent.action == ACTION_NOTIFICATION_CLICKED_SERVICE) {
             logEvent("click_noti_deactivate")
+            homeFragment.turnOffDetective()
             changeUITools(binding.viewpagerMain.currentItem, 0)
             binding.viewpagerMain.setCurrentItem(0, false)
             binding.ivHome.isSelected = false
