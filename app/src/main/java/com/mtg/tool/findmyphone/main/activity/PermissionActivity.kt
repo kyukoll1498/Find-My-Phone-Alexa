4d63cad18718cb152a1c6b37710ba54e6f9eb236
TuanAnh <tuananhxmen369@gmail.com>
2023-09-22 13:51:46 +0700

    Fix status switchButton when active permission record

--- app/src/main/java/com/mtg/tool/findmyphone/main/activity/PermissionActivity.kt
+++ app/src/main/java/com/mtg/tool/findmyphone/main/activity/PermissionActivity.kt
@@ -1,11 +1,8 @@
 package com.mtg.tool.findmyphone.main.activity
 
 
+import android.Manifest
 import android.content.Intent
-import android.content.res.ColorStateList
-import android.util.Log
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import com.common.control.manager.AdmobManager
 import com.common.control.manager.AppOpenManager
 import com.mtg.tool.findmyphone.BuildConfig
@@ -13,12 +10,9 @@ import com.mtg.tool.findmyphone.R
 import com.mtg.tool.findmyphone.REQUEST_MICRO_PERMISSION_CODE
 import com.mtg.tool.findmyphone.base.BaseActivity
 import com.mtg.tool.findmyphone.databinding.ActivityPermissionBinding
-import com.mtg.tool.findmyphone.main.dialog.RecordPermissionDialog
 import com.mtg.tool.findmyphone.utils.PermissionUtils
 
 class PermissionActivity : BaseActivity<ActivityPermissionBinding>(ActivityPermissionBinding::inflate) {
-
-//    private var hasMicrophonePermission = false
 
     override fun initView() {
 
@@ -37,14 +31,9 @@ class PermissionActivity : BaseActivity<ActivityPermissionBinding>(ActivityPermi
         AppOpenManager.getInstance().hideNativeOrBannerWhenShowOpenApp(this, binding.frAd)
 
         binding.llContinue.setOnClickListener {
-//            if (PermissionUtils.checkMicroPermission(mContext)) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-//            } else {
-//                Toast.makeText(this, getString(R.string.toast_permission_continue), Toast.LENGTH_SHORT).show()
-//            }
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
-
     }
 
     override fun addEvent() {
@@ -54,13 +43,25 @@ class PermissionActivity : BaseActivity<ActivityPermissionBinding>(ActivityPermi
     }
 
     private fun checkPermissionMicro() {
-        if (!PermissionUtils.checkMicroPermission(mContext)) {
+        if (!PermissionUtils.checkMicroPermission(mContext) && !shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)) {
             PermissionUtils.requestMicroPermission(this)
         } else {
-            Log.d("Error Permission Micro", "Check Permission")
-//            hasMicrophonePermission = true
-//            updateLLContinueBackground()
+            PermissionUtils.goSettingsForMicroPermission(this)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (PermissionUtils.checkMicroPermission(mContext)) {
+            turnOnSwitchButton()
+        }
+    }
+
+    private fun turnOnSwitchButton() {
+        binding.sbPermission.isEnabled = true
+        binding.sbPermission.isClickable = true
+        binding.sbPermission.isChecked = true
+        binding.sbPermission.isEnabled = false
     }
 
     override fun onRequestPermissionsResult(
@@ -75,10 +76,7 @@ class PermissionActivity : BaseActivity<ActivityPermissionBinding>(ActivityPermi
                 return
             } else {
                 // Auto checked sbPermission when allow permission record micro
-                binding.sbPermission.isEnabled = true
-                binding.sbPermission.isClickable = true
-                binding.sbPermission.isChecked = true
-                binding.sbPermission.isEnabled = false
+                turnOnSwitchButton()
 //                hasMicrophonePermission = true
 
                 // Change color button ll_continue
@@ -86,32 +84,4 @@ class PermissionActivity : BaseActivity<ActivityPermissionBinding>(ActivityPermi
             }
         }
     }
-
-//    private fun showRecordPermissionDialog() {
-//        RecordPermissionDialog(this) { granted ->
-//            if (granted) {
-//                hasMicrophonePermission = true
-//                updateLLContinueBackground()
-//
-//                binding.sbPermission.isEnabled = true
-//                binding.sbPermission.isClickable = true
-//                binding.sbPermission.isChecked = true
-//                binding.sbPermission.isEnabled = false
-//
-//                PermissionUtils.goSettingsForMicroPermission(this)
-//            }
-//        }.show()
-//    }
-
-//    private fun updateLLContinueBackground() {
-//        if (hasMicrophonePermission) {
-//            binding.llContinue.backgroundTintList = ColorStateList.valueOf(
-//                ContextCompat.getColor(this, R.color.txt_inactive2)
-//            )
-//        } else {
-//            binding.llContinue.backgroundTintList = ColorStateList.valueOf(
-//                ContextCompat.getColor(this, R.color.gray_continue)
-//            )
-//        }
-//    }
 }
