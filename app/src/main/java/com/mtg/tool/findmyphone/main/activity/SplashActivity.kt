33bbf346877b9b8d764f3c40dfcd07ee31b2223b
AnhNT <mtgtest3.vn@gmail.com>
2024-08-13 17:50:49 +0700

    Update time splash

--- app/src/main/java/com/mtg/tool/findmyphone/main/activity/SplashActivity.kt
+++ app/src/main/java/com/mtg/tool/findmyphone/main/activity/SplashActivity.kt
@@ -46,7 +46,7 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding
     override fun initView() {
         Handler().postDelayed(Runnable {
             canNextScreen.value = true
-        }, 500)
+        }, 15000)
         AppSession.isCompletedInterSplash = false
         logEvent("view_splash")
         ConsentDialogManager.instance?.showConsentDialogSplash(this) {
