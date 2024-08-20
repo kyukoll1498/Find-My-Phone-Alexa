fedbcd99bba7b48c08f41b84e1ddd32453300dd8
NhatNH <mtgtest3.vn@gmail.com>
2024-08-20 08:22:50 +0700

    update libs

--- MTGLibs/src/main/java/com/common/control/manager/AdmobManager.java
+++ MTGLibs/src/main/java/com/common/control/manager/AdmobManager.java
@@ -722,6 +722,11 @@ public class AdmobManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        try {
+            adView.setAdChoicesView(adView.findViewById(R.id.ad_choices));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         try {
             adView.setBodyView(adView.findViewById(R.id.ad_body));
